f61d0b0220c4325644d620431ba86fd02b205e4f
naveenchander96 <chandernaveen96@gmail.com>
2017-06-28 22:50:20 +0530

    Add files via upload

---
+++ Factorial.java
@@ -0,0 +1,21 @@
+package NAVEEN;
+import java.util.Scanner;
+public class Factorial {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+	int i=7,j,k=1;
+	for(j=1;j<=7;j++)
+	{
+		k=k*j;
+	}
+	System.out.println(k);
+	
+	
+	
+
+	}
+
+}
