585827308039e8035e34dcf660844286dff97f51
naveenchander96 <chandernaveen96@gmail.com>
2017-06-29 16:35:46 +0530

    Add files via upload

---
+++ Sum1.java
@@ -0,0 +1,22 @@
+import java.util.Scanner;
+public class Sum1 {
+
+	public static void main(String[] args) {
+	int a,b;
+	int c=1;
+	for(a=1;a<=15;a++)
+	{
+		c=c+a;
+	}
+	System.out.println(c);
+	for(a=15;a<=45;a++)
+	{
+		if(a%2==1)
+		{
+			System.out.println(a);
+		}
+	}
+
+	}
+
+}
