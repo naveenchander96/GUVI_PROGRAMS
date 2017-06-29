fc9983db53a54f49802af18db780d9b1ccb30743
naveenchander96 <chandernaveen96@gmail.com>
2017-06-29 22:18:13 +0530

    Add files via upload

---
+++ Swap1.java
@@ -0,0 +1,18 @@
+
+public class Swap1 {
+
+	
+	public static void main(String[] args) {
+		int a=4;
+		int b=5;
+		int c;
+		c=a;
+		a=b;
+		b=c;
+		System.out.println(a);
+		System.out.println(b);
+		
+
+	}
+
+}
