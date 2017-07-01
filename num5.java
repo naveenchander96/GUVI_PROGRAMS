868eca8d9ca2391bb0c80c517c23abfcf7e5ca5d
naveenchander96 <chandernaveen96@gmail.com>
2017-07-02 00:25:22 +0530

    Add files via upload

---
+++ num5.java
@@ -0,0 +1,16 @@
+import java.util.Scanner;
+public class Rev_number {
+
+	
+	public static void main(String[] args) {
+		int a;
+		Scanner obj=new Scanner(System.in);
+		a=obj.nextInt();
+		while(a)
+		{
+			
+		}
+			
+	}
+
+}
