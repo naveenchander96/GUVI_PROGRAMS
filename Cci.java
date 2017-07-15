c07146a93e8f29c0b2eb5db3a0feaac85f28e108
naveenchander96 <chandernaveen96@gmail.com>
2017-07-15 16:48:56 +0530

    Add files via upload

---
+++ Cci.java
@@ -0,0 +1,25 @@
+package Player;
+import java.util.Scanner;
+public class Cci {
+
+	public static void main(String[] args) {
+		int a=0;
+		int b=1;
+		int n;
+		Scanner obj=new Scanner(System.in);
+		n=obj.nextInt();
+	    System.out.print(a);
+	    System.out.print(b);
+	    for(int c=0;c<n;c++)
+	    {
+	    	int ans=a+b;
+	    	a=b;
+	    	b=ans;
+	    	
+	    	System.out.print(ans);
+	    	
+	    }
+
+	}
+
+}
